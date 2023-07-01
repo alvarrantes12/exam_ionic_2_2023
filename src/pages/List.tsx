74fe931b1287df11ea160f3f40ebcfdfea6c091a
tyrsergio <sergio.hernandezjimenez@ucr.ac.cr>
2023-07-01 11:38:18 -0600

    SHJ-C13656_DHA-B73808 ExamWorkingProgress

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,12 +1,62 @@
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle, 
+  IonContent,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonCardContent,
+  IonItem,
+  IonLabel,
+  IonSpinner,
+  IonButton
+} from "@ionic/react";
+import ApiMethods from '../commons/ApiMethods';
+import { environment } from "../environments/environment.dev";
+import "./List.css";
 
-import { useParams } from 'react-router';;
-import './List.css';
 
 const List: React.FC = () => {
 
-  return (
-    <div>Hola</div>
-  );
+  const {data} = ApiMethods(`${environment.apiEndpoint}/countries`)
+
+  if(!data){
+    return (
+      <div className="container">
+        <h1 className="Ion__Loading">Cargando...</h1>
+      </div>
+    )
+    
+
+  }else{
+    return (
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>
+              Examen 2
+            </IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent>
+          {data?.map((countrie: any) => {
+            return (
+              <IonCard className='Ion__Card'>
+                <IonCardHeader>
+                  <IonCardTitle className='Ion__Card__Title'>ID: {countrie.id}</IonCardTitle>
+                  <IonCardTitle className='Ion__Card__Title'>Nombre del país: {countrie.name}</IonCardTitle>
+                  <IonCardSubtitle className='Ion__Card__Subtitle'>Dato curioso: {countrie.fact}</IonCardSubtitle>
+                </IonCardHeader>
+              </IonCard>
+            )
+          })}
+        </IonContent>
+      </IonPage>
+    )
+  }
 };
 
 export default List;
