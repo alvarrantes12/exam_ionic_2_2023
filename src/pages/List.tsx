58dfd134dbc067e8eac14e7464e85172371db61c
KevinJAM2314 <kevin.arroyomora@ucr.ac.cr>
2023-07-01 09:40:34 -0600

    KJAM-C10724_JON-B97126 Added index, student 1

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,12 +1,50 @@
+import { useHistory } from 'react-router';
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonButton
 
-import { useParams } from 'react-router';;
-import './List.css';
+} from '@ionic/react'
+import ApiMethods from '../commons/ApiMethods';
+import { environment } from '../environments/environment.dev';
 
 const List: React.FC = () => {
-
-  return (
-    <div>Hola</div>
-  );
+  const history = useHistory();
+  const { data } = ApiMethods(`${environment.apiEndpoint}/countries`);
+  if(!data){
+    return <h1>Cargando Informacion...</h1>
+  } else{
+    return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>
+            Paises disponibles
+          </IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        { data?.map((country: any) => {
+          return(
+            <IonCard>
+              <IonCardHeader>
+                <IonCardTitle >Nombre: {country.name}</IonCardTitle>
+                <IonCardSubtitle >Hecho: {country.fact}</IonCardSubtitle>
+              </IonCardHeader>
+            </IonCard>
+          )
+        })}
+      </IonContent>
+    </IonPage>
+    )
+  }
 };
 
 export default List;
