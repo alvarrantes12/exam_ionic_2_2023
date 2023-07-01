da290022efc9c2b648e7bd8670546ab8d0341301
JoseIsaacLazo <jose.lazo@ucrso.info>
2023-07-01 11:12:36 -0600

    JILL-C04168 Second commit Ionic

--- src/pages/form/Form.tsx
+++ src/pages/form/Form.tsx
@@ -1,17 +1,30 @@
 import React, { useState } from 'react';
+import ApiMethods from '../../commons/ApiMethods';
+import { useHistory } from 'react-router-dom';
+import { environment } from '../../environments/environment.dev';
 import { IonContent, IonInput, IonItem, IonLabel, IonPage, IonButton } from '@ionic/react';
 
 const Form: React.FC = () => {
     const [id, setId] = useState('');
     const [name, setName] = useState('');
+    const [fact, setFact] = useState('');
     const [message, setMessage] = useState('');
+    const history = useHistory();
+
+
+    const { putMethod } = ApiMethods(`${environment.apiEndpoint}/countries`);
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
-        putMethod(id, name);
+        putMethod(id, name, fact);
         setMessage('Editado correctamente');
         setId('');
         setName('');
+    }
+
+    const handleRedirect = () => {
+        history.push('/pages/List');
+        window.location.reload();
     }
 
     return (
@@ -26,16 +39,26 @@ const Form: React.FC = () => {
                             onIonChange={(e) => setId(e.detail.value!)}
                             required></IonInput>
                     </IonItem>
-                    <IonItem style={{marginBottom: '20px'}}>
-                        <IonLabel position='floating'>Nombre</IonLabel>
+                    <IonItem>
+                        <IonLabel position='floating'>Nombre del país</IonLabel>
                         <IonInput
                             value={name}
                             onIonChange={(e) => setName(e.detail.value!)}
                             required></IonInput>
                     </IonItem>
-                    <IonButton type='submit' expand='full'>Editar</IonButton>
+                    <IonItem style={{marginBottom: '20px'}}>
+                        <IonLabel position='floating'>Dato curioso del país </IonLabel>
+                        <IonInput
+                            value={fact}
+                            onIonChange={(e) => setFact(e.detail.value!)}
+                            required></IonInput>
+                    </IonItem>
+                    <IonButton type='submit' expand='full'>Editar país</IonButton>
                 </form>
-                <IonButton expand='full'>Ir a la Lista</IonButton>
+                <IonButton onClick={handleRedirect} expand='full' style={{
+                        marginLeft: '22px',
+                        marginRight: '12px'
+                    }} >Ir a la Lista de países</IonButton>
             </IonContent>
         </IonPage>
     )
