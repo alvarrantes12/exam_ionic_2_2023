cc4419c335a0bc75b5e5639f29f4caa8d38dd7e9
alexavegarojas <alexavr92@gmail.com>
2023-07-01 11:27:15 -0600

    AVR-C09317 Exam 2 point 5 done Ionic

--- src/commons/ApiMethods.tsx
+++ src/commons/ApiMethods.tsx
@@ -1,10 +1,26 @@
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function ApiMethods(url: any) {
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
+
+    useEffect (() => {
+	    const config = {
+		    headers: {
+			    'Accept': 'application/json',
+			    'Content-Type': 'application/json'
+		    }
+	    }
+
+	    setLoading(true);
+	    axios.get(url, config)
+		    .then((response) => { setData(response.data) })
+		    .catch((err) => {setError(err) })
+		    .finally(() => {setLoading(false) })
+
+    }, [url])
 
     const putMethod = (id: any, name: any) => {
         const config = {
--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,12 +1,50 @@
-
-import { useParams } from 'react-router';;
 import './List.css';
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonButton
+} from '@ionic/react';
+import { environment } from '../environments/environment.dev';
+import ApiMethods from '../commons/ApiMethods';
 
 const List: React.FC = () => {
+  const { data} = ApiMethods(`${environment.apiEndpoint}/countries`);
+  if (!data){
+    return <h1> Cargando información</h1>
+  } else {
+    return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>
+            Examen II
+            C09317 - C01950
+          </IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        {data?.map((country: any) => {
+            return(
+            <IonCard className= 'Ion__Card'>
+              <IonCardHeader>
+                  <IonCardTitle className= 'Ion__Card__Title'> Nombre: {country.name} </IonCardTitle>
+                  <IonCardSubtitle className= 'Ion__Card__SubTitle'> Dato:  {country.fact}</IonCardSubtitle>
+              </IonCardHeader>
+            </IonCard>
+            )
+        })}
+      </IonContent>
+    </IonPage>
+    )
+  }
 
-  return (
-    <div>Hola</div>
-  );
 };
 
 export default List;
