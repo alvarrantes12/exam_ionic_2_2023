ecbc97b5e5fb803e5ff09d92d45629374990bf10
KendallC09 <kendallconejo00@gmail.com>
2023-07-01 10:04:15 -0600

    KCA-C02310 first Ionic commit done

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,12 +1,50 @@
-
-import { useParams } from 'react-router';;
 import './List.css';
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonCardContent,
+  IonButton
+} from '@ionic/react';
+import ApiMethods from '../commons/ApiMethods';
+import { environment } from '../environments/environment.dev';
 
 const List: React.FC = () => {
+  const {data, refetch} = ApiMethods(`${environment.apiEndpoint}/countries`);
 
-  return (
-    <div>Hola</div>
-  );
-};
+  if (!data) {
+    return <h1>Cargando...</h1>
+  } else {
+    return (
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>
+              Países
+            </IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent>
+          {data?.map((country: any) => {
+            return (
+              <IonCard className='IonCard'>
+                <IonCardHeader>
+                  <IonTitle className='IonCardTitle'>Nombre: {country.name}</IonTitle>
+                  <IonCardSubtitle className='IonCardSubtitle'>Dato Curioso: {country.fact}</IonCardSubtitle>
+                </IonCardHeader>
+              </IonCard>
+            )
+          })}
+        </IonContent>
+      </IonPage>
+    )
+  }
+}
 
 export default List;
