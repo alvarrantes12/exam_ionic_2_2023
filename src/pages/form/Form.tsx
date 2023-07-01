cccce64d845405e16115beb6aaed8439be1c8c03
jesus <jeadroar05@gmail.com>
2023-07-01 11:02:15 -0600

    IVG-C18512_JRA-C16536 Part 2 ionic by Jesus

--- src/pages/form/Form.tsx
+++ src/pages/form/Form.tsx
@@ -1,10 +1,16 @@
 import React, { useState } from 'react';
 import { IonContent, IonInput, IonItem, IonLabel, IonPage, IonButton } from '@ionic/react';
+import ApiMethods from '../../commons/ApiMethods';
+import { environment } from '../../environments/environment.dev';
+import { useHistory } from 'react-router-dom';
 
 const Form: React.FC = () => {
     const [id, setId] = useState('');
     const [name, setName] = useState('');
     const [message, setMessage] = useState('');
+    const history = useHistory();
+
+    const {putMethod} = ApiMethods(`${environment.apiEndpoint}/countries`)
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
@@ -12,6 +18,11 @@ const Form: React.FC = () => {
         setMessage('Editado correctamente');
         setId('');
         setName('');
+    }
+
+    const handleRedirect = () => {
+        history.push('/pages/List');
+        window.location.reload();
     }
 
     return (
@@ -35,7 +46,7 @@ const Form: React.FC = () => {
                     </IonItem>
                     <IonButton type='submit' expand='full'>Editar</IonButton>
                 </form>
-                <IonButton expand='full'>Ir a la Lista</IonButton>
+                <IonButton onClick={handleRedirect} expand='full'>Ir a la Lista</IonButton>
             </IonContent>
         </IonPage>
     )
